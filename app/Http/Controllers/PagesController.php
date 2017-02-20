3bb3e6bd3ca13f04198386f6abe7e8ceb517df8e
Guido Contreras Woda <guiwoda@gmail.com>
2017-02-20 17:48:44 -0500

    Video 8

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,34 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Message;
 use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
     public function home()
     {
-    	$messages = [
-            [
-                'id' => 1,
-                'content' => 'Este es mi primer mensaje!',
-                'image' => 'http://lorempixel.com/600/338?1',
-            ],
-            [
-                'id' => 2,
-                'content' => 'Este es mi segundo mensaje!',
-                'image' => 'http://lorempixel.com/600/338?2',
-            ],
-            [
-                'id' => 3,
-                'content' => 'Otro mensaje más!',
-                'image' => 'http://lorempixel.com/600/338?3',
-            ],
-            [
-                'id' => 4,
-                'content' => 'El ultimo mensaje!',
-                'image' => 'http://lorempixel.com/600/338?4',
-            ],
-        ];
+    	$messages = Message::all();
 
 	    return view('welcome', [
 	    	'messages' => $messages,
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,10 +14,10 @@
 <div class="row">
 	@forelse($messages as $message)
 		<div class="col-6">
-			<img class="img-thumbnail" src="{{ $message['image'] }}">
+			<img class="img-thumbnail" src="{{ $message->image }}">
 			<p class="card-text">
-				{{ $message['content'] }}
-				<a href="/messages/{{ $message['id'] }}">Leer más</a>
+				{{ $message->content }}
+				<a href="/messages/{{ $message->id }}">Leer más</a>
 			</p>
 		</div>
 	@empty
