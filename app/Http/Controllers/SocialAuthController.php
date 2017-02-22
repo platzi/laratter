4eb209dc6917ce65668b415f00e8451aa2577e5d
Guido Contreras Woda <guiwoda@gmail.com>
2017-02-22 17:15:27 -0500

    Video 23

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -18,6 +18,16 @@ class SocialAuthController extends Controller
     {
     	$user = Socialite::driver('facebook')->user();
 
+        $existing = User::whereHas('socialProfiles', function ($query) use ($user) {
+            $query->where('social_id', $user->id);
+        })->first();
+
+        if ($existing !== null) {
+            auth()->login($existing);
+
+            return redirect('/');
+        }
+
     	session()->flash('facebookUser', $user);
 
     	return view('users.facebook', [
