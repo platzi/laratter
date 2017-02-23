1a84634a967fe625643f5a6ad824cc74fc5a0da4
Guido Contreras Woda <guiwoda@gmail.com>
2017-02-23 16:16:49 -0500

    Video #20
    - Test de status code
    - Test de contenido
    - Test con usuario creado en el test
    - Incorporo DatabaseTransactions
    - Test login
    - Incorporo InteractsWithDatabase
    - Test de escritura a base de datos

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\User;
+use Illuminate\Foundation\Testing\Concerns\InteractsWithDatabase;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -9,6 +11,9 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
+    use DatabaseTransactions;
+    use InteractsWithDatabase;
+
     /**
      * A basic test example.
      *
@@ -19,5 +24,46 @@ class ExampleTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    public function testSearchForMessages()
+    {
+        $response = $this->get('/messages?query=Alice');
+
+        $response->assertSee('Alice');
+    }
+
+    public function testCanSeeUserPage()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->get($user->username);
+
+        $response->assertSee($user->name);
+    }
+
+    public function testCanLogin()
+    {
+        $user = factory(User::class)->create();
+
+        $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'secret',
+        ]);
+
+        $this->seeIsAuthenticatedAs($user);
+    }
+
+    public function testCanFollow()
+    {
+        $user = factory(User::class)->create();
+        $other = factory(User::class)->create();
+
+        $response = $this->actingAs($user)->put($other->username);
+
+        $this->assertDatabaseHas('followers', [
+            'follower_id' => $user->id,
+            'user_id' => $other->id,
+        ]);
     }
 }
