d3fc0c7dd3dad8b93840476ab5b507e15422c752
Guido Contreras Woda <guiwoda@gmail.com>
2017-02-24 11:46:57 -0500

    Video 36

---
+++ resources/assets/js/components/Responses.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="row">
+		<a href="#" class="btn btn-outline-primary" v-on:click="load">Ver respuestas</a>
+
+		<div class="col-12 mt-2" v-for="response in responses">
+			<div class="card">
+				<div class="card-block">
+					{{ response.message }}
+				</div>
+				<div class="card-footer text-muted">
+					{{ response.created_at }}
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: ['message'],
+	data() {
+		return {
+			responses: []
+		}
+	},
+
+	methods: {
+		load() {
+			axios.get('/api/messages/' + this.message + '/responses')
+				.then(res => {
+					this.responses = res.data;
+				});
+		}
+	}
+}
+</script>
--- resources/views/messages/show.blade.php
+++ resources/views/messages/show.blade.php
@@ -3,4 +3,6 @@
 @section('content')
 <h1 class="h3">Mensaje id: {{ $message->id }}</h1>
 @include('messages.message')
+
+<responses :message="{{ $message->id }}"></responses>
 @endsection
