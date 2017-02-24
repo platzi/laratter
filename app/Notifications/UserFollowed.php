1cf2c11cb13606052e2b204f3cced6d007b35e95
Guido Contreras Woda <guiwoda@gmail.com>
2017-02-24 16:00:24 -0500

    Video 38

--- app/Notifications/UserFollowed.php
+++ app/Notifications/UserFollowed.php
@@ -32,7 +32,7 @@ class UserFollowed extends Notification
      */
     public function via($notifiable)
     {
-        return ['mail'];
+        return ['mail', 'database'];
     }
 
     /**
@@ -60,7 +60,7 @@ class UserFollowed extends Notification
     public function toArray($notifiable)
     {
         return [
-            //
+            'follower' => $this->follower,
         ];
     }
 }
